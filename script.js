4aa705f30128e9062147ba8dacf5401c0edf8f7f
Nadav <26648774+Nadavse@users.noreply.github.com>
2024-01-20 23:13:00 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -15,4 +15,39 @@ function getComputerChoice() {
 //    console.log(choice);
     return choice;
 };
-console.log(getComputerChoice());
+
+function playRound(playerSelection,ComputerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = ComputerSelection.toLowerCase();
+    let winner;
+    if (player === computer) {
+        winner = 'tie';
+    } else if (player === 'scissors' && computer === 'paper') {
+        winner = 'player';
+    } else if (player === 'scissors' && computer === 'rock') {
+        winner = 'computer';
+    } else if (player === 'paper' && computer === 'rock') {
+        winner = 'player';
+    } else if (player === 'paper' && computer === 'scissors') {
+        winner = 'computer';
+    } else if (player === 'rock' && computer === 'scissors') {
+        winner = 'player';
+    } else if (player === 'rock' && computer === 'paper') {
+        winner = 'computer';
+    }
+    switch (winner) {
+        case tie:
+            return `it's a tie! you both chose ${player}`;
+            break;
+        case player:
+            return `You Win! ${player} beats ${computer}`;
+            break;
+        case computer:
+            return `You Lose! ${computer} beats ${player}`;
+            break;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
